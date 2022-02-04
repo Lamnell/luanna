ab880aa251b64d98aa249dbcb7c3b968fd28468b
Lamnell <83544969+Lamnell@users.noreply.github.com>
2022-02-04 10:50:11 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+let menuMobile = document.querySelector('.menuMobile')
+let botaoMenu = document.querySelector('.botaoMenu')
+
+let aberto = false
+
+function abrirOuFecharMenu() {
+    
+    if (aberto === true) { 
+        aberto = false
+        menuMobile.classList.remove('aberto')
+        
+    } else {
+        aberto = true
+        menuMobile.classList.add('aberto') 
+        
+    }
+}
